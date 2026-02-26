d8b1bb8ac1844e108f09f6deddb696afd4102687
Yannis Moser <yannis.moser@sap.com>
2026-02-26 12:20:36 +0100

    Fixed Lint Error

--- tests/bookshop/srv/annotation-service.ts
+++ tests/bookshop/srv/annotation-service.ts
@@ -7,45 +7,61 @@ class AnnotationService extends cds.ApplicationService {
 
     // Start actions
     this.on('triggerStart', 'StartOnAction', async (req: cds.Request) => {
-      const entity = await SELECT.one.from('AnnotationService.StartOnAction').where(req.params[0] as object);
+      const entity = await SELECT.one
+        .from('AnnotationService.StartOnAction')
+        .where(req.params[0] as object);
       return entity;
     });
 
     this.on('triggerStartWhen', 'StartOnActionWhen', async (req: cds.Request) => {
-      const entity = await SELECT.one.from('AnnotationService.StartOnActionWhen').where(req.params[0] as object);
+      const entity = await SELECT.one
+        .from('AnnotationService.StartOnActionWhen')
+        .where(req.params[0] as object);
       return entity;
     });
 
     // Cancel actions
     this.on('triggerCancel', 'CancelOnAction', async (req: cds.Request) => {
-      const entity = await SELECT.one.from('AnnotationService.CancelOnAction').where(req.params[0] as object);
+      const entity = await SELECT.one
+        .from('AnnotationService.CancelOnAction')
+        .where(req.params[0] as object);
       return entity;
     });
 
     this.on('triggerCancelWhen', 'CancelOnActionWhen', async (req: cds.Request) => {
-      const entity = await SELECT.one.from('AnnotationService.CancelOnActionWhen').where(req.params[0] as object);
+      const entity = await SELECT.one
+        .from('AnnotationService.CancelOnActionWhen')
+        .where(req.params[0] as object);
       return entity;
     });
 
     // Suspend actions
     this.on('triggerSuspend', 'SuspendOnAction', async (req: cds.Request) => {
-      const entity = await SELECT.one.from('AnnotationService.SuspendOnAction').where(req.params[0] as object);
+      const entity = await SELECT.one
+        .from('AnnotationService.SuspendOnAction')
+        .where(req.params[0] as object);
       return entity;
     });
 
     this.on('triggerSuspendWhen', 'SuspendOnActionWhen', async (req: cds.Request) => {
-      const entity = await SELECT.one.from('AnnotationService.SuspendOnActionWhen').where(req.params[0] as object);
+      const entity = await SELECT.one
+        .from('AnnotationService.SuspendOnActionWhen')
+        .where(req.params[0] as object);
       return entity;
     });
 
     // Resume actions
     this.on('triggerResume', 'ResumeOnAction', async (req: cds.Request) => {
-      const entity = await SELECT.one.from('AnnotationService.ResumeOnAction').where(req.params[0] as object);
+      const entity = await SELECT.one
+        .from('AnnotationService.ResumeOnAction')
+        .where(req.params[0] as object);
       return entity;
     });
 
     this.on('triggerResumeWhen', 'ResumeOnActionWhen', async (req: cds.Request) => {
-      const entity = await SELECT.one.from('AnnotationService.ResumeOnActionWhen').where(req.params[0] as object);
+      const entity = await SELECT.one
+        .from('AnnotationService.ResumeOnActionWhen')
+        .where(req.params[0] as object);
       return entity;
     });
 
