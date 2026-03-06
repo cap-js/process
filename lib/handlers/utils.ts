c35d977304d1b6fa115068ca341f328c5e438f08
Til Weber <til.weber@sap.com>
2026-03-06 13:16:20 +0100

    updated imports from merge

--- lib/handlers/utils.ts
+++ lib/handlers/utils.ts
@@ -1,23 +1,12 @@
 import cds, { column_expr, expr, Target } from '@sap/cds';
-import { PROCESS_LOGGER_PREFIX, PROCESS_SERVICE } from '../constants';
-import cds, { column_expr, expr, Results, Target } from '@sap/cds';
 import {
   BUSINESS_KEY_HEADER_INFO,
   BUSINESS_KEY_HEADER_INFO_BPM,
   BUSINESS_KEY_SEMANTIC_KEY,
   BUSINESS_KEY_SEMANTIC_KEY_BPM,
-  PROCESS_CANCEL_IF,
-  PROCESS_CANCEL_ON,
   PROCESS_LOGGER_PREFIX,
-  PROCESS_RESUME_IF,
-  PROCESS_RESUME_ON,
   PROCESS_SERVICE,
-  PROCESS_START_IF,
-  PROCESS_START_ON,
-  PROCESS_SUSPEND_IF,
-  PROCESS_SUSPEND_ON,
 } from '../constants';
-import { getColumnsForProcessStart } from './processStart';
 const { SELECT } = cds.ql;
 const LOG = cds.log(PROCESS_LOGGER_PREFIX);
 
