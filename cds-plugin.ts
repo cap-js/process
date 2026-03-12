fc11acf75fb6f0f1ddf8fbdb2affc4ad5f43cd7c
Yannis Moser <yannis.moser@sap.com>
2026-03-12 17:22:24 +0100

    changed kind

--- cds-plugin.ts
+++ cds-plugin.ts
@@ -32,7 +32,7 @@ cds.import ??= {};
 //@ts-expect-error: cds type does not exist
 cds.import.options ??= {};
 //@ts-expect-error: cds type does not exist
-cds.import.options.process = { no_copy: true, as: 'cds', config: 'kind=rest' };
+cds.import.options.process = { no_copy: true, as: 'cds', config: 'kind=process-service' };
 // @ts-expect-error: process does not exist on cds.import type
 cds.import.from ??= {};
 // @ts-expect-error: from does not exist on cds.import type
