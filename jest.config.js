63513ad167806a213f67198d7f29d0cb5f7af1ea
Yannis Moser <yannis.moser@sap.com>
2026-03-16 13:22:51 +0100

    excluding programmatic hybrid tests and adding all the other tests for e2e tests

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ const config = {
   testEnvironment: 'node',
   testTimeout: 60000,
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/hybrid/programmaticApproach.test.ts'],
   forceExit: true,
   detectOpenHandles: true,
   extensionsToTreatAsEsm: ['.ts'],
